4d94b588cabe6a1a4b9791b732bb9c8d1344c215
peter xu <peter1123@qq.com>
2022-05-14 23:02:38 +0800

    Update openvpn_script_createuser.py

--- openvpn_script_createuser.py
+++ openvpn_script_createuser.py
@@ -1,8 +1,9 @@
 import csv
 import os
 
-csvfile= open('/tmp/VPN.csv','r')
+csvfile= open('/tmp/VPN.csv','r',encoding='utf8')
 reader = csv.DictReader(csvfile)
 
 for row in reader:
-    os.system('/usr/local/openvpn_as/scripts/sacli --user '+ row['username']+ ' --new_pass='+ row['password'] + ' --key "conn_group" --value '+ row['group']+' UserPropPut')
+    os.system('/usr/sbin/sacli --user '+ row['username']+ ' --new_pass='+ row['password'] + ' --key "conn_group" --value '+ row['group']+' UserPropPut')
+
