32453efd2cd9600725a2e6ca60d33e810595dfb4
peter xu <peter1123@qq.com>
2022-05-15 12:52:46 +0800

    Update openvpn_script_createuser.py
    pip install paramiko

--- openvpn_script_createuser.py
+++ openvpn_script_createuser.py
@@ -1,7 +1,14 @@
 import csv
-import os
+import paramiko
+
+client = paramiko.SSHClient()
+client.load_system_host_keys()
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+#please change your username & password
+client.connect(hostname='ipaddress', port=22, username='username', password='password')
 
 with open ('VPN.csv','r',encoding='utf8') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
-        os.system('/usr/sbin/sacli --user '+ row['username']+ ' --new_pass='+ row['password'] + ' --key '"conn_group"' --value '+ row['group']+' UserPropPut')
+        stdin, stdout, stderr = client.exec_command('/usr/sbin/sacli --user '+ row['username']+ ' --key '"conn_group"' --value '+ row['group']+' UserPropPut;/usr/sbin/sacli --user '+ row['username']+ ' --new_pass='+ row['password'] +' SetLocalPassword')
+stdin.close()
