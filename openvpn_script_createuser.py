e22f13aedbe8c33c60471a9e921ff0095c39138b
peter xu <peter1123@qq.com>
2022-05-18 15:59:07 +0800

    Update openvpn_script_createuser.py
    print stdout & stderr

--- openvpn_script_createuser.py
+++ openvpn_script_createuser.py
@@ -15,5 +15,12 @@ client.connect(host,port,username,password)
 with open ('VPN.csv','r',encoding='utf8') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
-        stdin, stdout, stderr = client.exec_command('/usr/sbin/sacli --user '+ row['username']+ ' --key '"conn_group"' --value '+ row['group']+' UserPropPut;/usr/sbin/sacli --user '+ row['username']+ ' --new_pass='+ row['password'] +' SetLocalPassword')
+        command1 = f"""/usr/sbin/sacli --user {row['username']} --key "conn_group" --value {row['group']} UserPropPut"""
+        command2 = f"""/usr/sbin/sacli --user {row['username']} --new_pass={row['password']} SetLocalPassword"""
+        stdin, stdout, stderr = client.exec_command(f"{command1} && {command2}")
+        print(stdout.read().decode())
+        err=stderr.read().decode()
+        if err:
+            print(err)
+
 stdin.close()
